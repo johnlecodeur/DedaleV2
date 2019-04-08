fe8295888f2a72063fe59bce79bb420d8c15f5ba
John <John@DESKTOP-5H4ILI8>
2019-04-08 20:00:01 +0200

    class case créée, un champ agent sera peut être nécéssaire

--- DedaleEtuFull201/DedaleEtuFull2018/src/architecture/Case.java
+++ DedaleEtuFull201/DedaleEtuFull2018/src/architecture/Case.java
@@ -1,11 +1,19 @@
 package architecture;
 
-import java.sql.Date;
+import java.util.Date;
 import jade.util.leap.Serializable;
+
+
+
+
+//class reprensentant un noeud du graphe 
+
+
+
 
 public class Case implements Serializable{
 	private String id;
-	private boolean node_open=false;
+	private boolean node_open=true;//mis a true car si on cree la case c'est que le noeud est ouvert
 	private Date date;
 	//private Agent agent;
 	private int gold;
@@ -15,8 +23,67 @@ public class Case implements Serializable{
 	
 	
 	
-	public Case() {
-		
-		
+	public Case(String id,boolean node_open,/*Date date,*/int gold,int diamond,boolean tresor,boolean tresor_ouvert) {
+		this.id=id;
+		this.node_open=node_open;
+		date=new Date();
+		this.gold=gold;
+		this.diamond=diamond;
+		this.tresor=tresor;
+		this.tresor_ouvert=tresor_ouvert;
+		}
+	
+	//recupere l'id de la case
+	public String getId() {
+		return this.id;
+	}
+	
+	//change l'id de la case
+	public void setId(String id) {
+		this.id=id;
+	}
+	
+	public boolean is_Open() {
+		return this.node_open;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+/*	
+	public boolean is_Agent() {
+		return this.agent;
+	}
+*/	
+	public int getGold() {
+		return this.gold;
+	}
+	
+	public void SetGold(int gold) {
+		this.gold=gold;
+	}
+	
+	public int getDioamond() {
+		return this.diamond;
+	}
+	
+	public void SetDiamond(int diamond) {
+		this.diamond=diamond;
+	}
+	
+	public boolean is_Tresor() {
+		return this.tresor;
+	}
+	
+	public void SetTresor(boolean tresor) {
+		this.tresor=tresor;
+	}
+	
+	public boolean is_TresorOuvert() {
+		return this.tresor_ouvert;
+	}
+	
+	public void SetTresorOuvert(boolean tresor_ouvert) {
+		this.tresor_ouvert=tresor_ouvert;
 	}
 }
